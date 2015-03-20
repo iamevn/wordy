5719daac6f6010155815b06b0bff1e979158e458
iamevn <iamevn@users.noreply.github.com>
2015-03-20 13:57:05 -0700

    adding/deleting rows

--- gui test.rkt
+++ gui test.rkt
@@ -25,12 +25,14 @@
   (new menu-item%
        [label "&refresh cache"]
        [parent file-menu]
-       [callback (λ (m e) (refresh-cache))]))
+       [callback (λ (m e) (send frame set-status-text "refreshing cache")
+                   (refresh-cache))]))
 (define exit-item
   (new menu-item%
        [label "&exit"]
        [parent file-menu]
-       [callback (λ (m e) (send frame set-status-text "exit clicked"))]))
+       [callback (λ (m e) (send frame set-status-text "exit clicked")
+                   (exit))]))
 
 (define lines-menu
   (new menu%
@@ -40,17 +42,20 @@
   (new menu-item%
        [label "&add line"]
        [parent lines-menu]
-       [callback (λ (m e) (send frame set-status-text "adding a line"))]))
+       [callback (λ (m e) (send frame set-status-text "adding a line")
+                   (add-row num-rows))]))
 (define del-line-item
   (new menu-item%
        [label "&delete line"]
        [parent lines-menu]
-       [callback (λ (m e) (send frame set-status-text "deleting a line"))]))
+       [callback (λ (m e) (send frame set-status-text "deleting a line")
+                   (delete-row (sub1 num-rows)))]))
 (define add-line-before-num-item
   (new menu-item%
        [label "add a line before line number..."]
        [parent lines-menu]
-       [callback (λ (m e) (send frame set-status-text "adding a line before..."))]))
+       [callback (λ (m e) (send frame set-status-text "adding a line before...")
+                   )]))
 (define del-line-num-item
   (new menu-item%
        [label "delete line number..."]
@@ -63,16 +68,31 @@
        [style '(vscroll)]))
 ;; a row
 (define row%
-  (class horizontal-pane%
-    (init n)
+  (class horizontal-panel%
+    (init-field n)
     (super-new)
+    (public increment-row decrement-row get-row)
+    (define (increment-row)
+      (set! n (add1 n))
+      (refresh-line-num))
+    (define (decrement-row)
+      (set! n (sub1 n))
+      (refresh-line-num))
+    (define (get-row)
+      n)
     (define (generate-sentence)
       (let* ([cmd (string->symbol (send choice get-string-selection))]
              [sen (sentence-for-cmd cmd)])
         (send text set-value sen)))
+    (define (refresh-line-num)
+      (send line-num set-label (~a n": ")))
+    (define line-num
+      (new message%
+           [label (~a n": ")]
+           [parent this]))
     (define text
       (new text-field%
-           [label (~a n": ")]
+           [label #f]
            [parent this]))
     (define generate-button
       (new button%
@@ -94,5 +114,29 @@
                             )]
            [parent this]))))
 ;; list of rows
-(define rows (map (λ (n) (new row% [parent panel] [n n])) (build-list 10 values)))
+(define num-rows 10)
+(define rows (map (λ (n) (new row% [parent panel] [n n])) (build-list num-rows values)))
 (send frame show #t)
+
+;add a row with line number n
+(define (add-row n)
+  ;every row with a line number greater-than or equal to n gets its line number incremented
+  (map (λ (row) (send row increment-row) )
+       (filter (λ (row) (>= (send row get-row) n))
+               rows))
+  ;add the row
+  (set! num-rows (add1 num-rows))
+  (set! rows (sort (cons (new row% [parent panel] [n n]) rows)
+                   < #:key (λ (row) (send row get-row))))
+  ;tell the panel to resort its children
+  (send panel change-children (λ (c) rows)))
+;delete row with line number n
+(define (delete-row n)
+  ;remove the row
+  (set! rows (remove n rows (λ (n row) (equal? n (send row get-row)))))
+  ;every row with line number greater-than n gets its line number decremented
+  (map (λ (row) (send row decrement-row))
+       (filter (λ (row) (> (send row get-row) n))
+               rows))
+  (send panel change-children (λ (c) rows))
+  (set! num-rows (sub1 num-rows)))
